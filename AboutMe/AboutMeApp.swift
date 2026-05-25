46da9380f44824242a5e3e1648202fbffd44e8a0
Jose Marquez <jose.marquez012@mymdc.net>
2026-05-25 16:45:58 -0400

    Background update fix 3

--- AboutMe/AboutMeApp.swift
+++ AboutMe/AboutMeApp.swift
@@ -10,9 +10,17 @@ See the License.txt file for this sample’s licensing information.
 */
 
 import SwiftUI
+import UIKit
 
 @main
 struct AboutMeApp: App {
+    init() {
+        // Ensure UIKit containers don't paint an opaque white background over SwiftUI.
+        UIView.appearance().backgroundColor = .clear
+        UITabBar.appearance().backgroundColor = .clear
+        UITabBar.appearance().isTranslucent = true
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()

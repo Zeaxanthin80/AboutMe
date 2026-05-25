cde832c08143670537cc012588e711dc4b8774ea
Jose Marquez <jose.marquez012@mymdc.net>
2026-05-25 17:19:33 -0400

    background colors restored

--- AboutMe/StoryView.swift
+++ AboutMe/StoryView.swift
@@ -14,7 +14,11 @@ import SwiftUI
 struct StoryView: View {
     var body: some View {
         ZStack {
-            Color.clear
+            LinearGradient(
+                colors: [Color("LightR"), Color(red: 0.42, green: 0.32, blue: 0.27), Color("DarkR")],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
                 .ignoresSafeArea()
             
             VStack {

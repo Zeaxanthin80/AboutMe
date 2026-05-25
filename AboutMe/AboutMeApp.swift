9e1d1d430cbed9d075d1929cd5b3be07a2bfe46e
Jose Marquez <jose.marquez012@mymdc.net>
2026-05-25 17:14:09 -0400

    update 5

--- AboutMe/AboutMeApp.swift
+++ AboutMe/AboutMeApp.swift
@@ -1,24 +1,13 @@
-//
-//  AboutMeApp.swift
-//  AboutMe
-//
-//  Created by user on 5/24/26.
-//
-
-/*
-See the License.txt file for this sample’s licensing information.
-*/
-
 import SwiftUI
-import UIKit
 
 @main
 struct AboutMeApp: App {
     init() {
-        // Ensure UIKit containers don't paint an opaque white background over SwiftUI.
-        UIView.appearance().backgroundColor = .clear
-        UITabBar.appearance().backgroundColor = .clear
-        UITabBar.appearance().isTranslucent = true
+        let appearance = UITabBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor = .clear
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
     }
 
     var body: some Scene {
@@ -27,7 +16,3 @@ struct AboutMeApp: App {
         }
     }
 }
-#Preview {
-    ContentView()
-}
-
